52fbd0bdb82939185280f6133f2c94dc93b08561
christopher.walker <christopher.walker@digital.hmrc.gov.uk>
2017-08-20 19:58:45 +0100

    refactored to use new cjww deps

--- build.sbt
+++ build.sbt
@@ -13,13 +13,13 @@ scalaVersion := "2.11.11"
 organization := "com.cjww-dev.libs"
 
 val cjwwDep: Seq[ModuleID] = Seq(
-  "com.cjww-dev.libs" %% "http-verbs" % "2.1.0",
-  "com.cjww-dev.libs" %% "data-security" % "2.5.0",
-  "com.cjww-dev.libs" %% "application-utilities" % "1.2.0"
+  "com.cjww-dev.libs" %% "http-verbs" % "2.2.0",
+  "com.cjww-dev.libs" %% "data-security" % "2.6.1",
+  "com.cjww-dev.libs" %% "application-utilities" % "2.0.1"
 )
-val codeDep: Seq[ModuleID] = Seq("com.typesafe.play" % "play_2.11" % "2.5.15")
+val codeDep: Seq[ModuleID] = Seq("com.typesafe.play" % "play_2.11" % "2.5.16")
 val testDep: Seq[ModuleID] = Seq(
-  "org.scalatestplus.play" % "scalatestplus-play_2.11" % "2.0.0",
+  "org.scalatestplus.play" % "scalatestplus-play_2.11" % "2.0.1",
   "org.mockito" % "mockito-core" % "2.8.47"
 )
 
--- src/main/scala/com/cjwwdev/auth/connectors/AuthConnector.scala
+++ src/main/scala/com/cjwwdev/auth/connectors/AuthConnector.scala
@@ -17,18 +17,21 @@ package com.cjwwdev.auth.connectors
 
 import javax.inject.{Inject, Singleton}
 
-import com.cjwwdev.auth.config.ApplicationConfiguration
 import com.cjwwdev.auth.models.AuthContext
 import com.cjwwdev.http.exceptions.NotFoundException
 import com.cjwwdev.http.utils.SessionUtils
 import com.cjwwdev.http.verbs.Http
+import com.typesafe.config.ConfigFactory
 import play.api.mvc.Request
 
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
 
 @Singleton
-class AuthConnector @Inject()(http: Http) extends ApplicationConfiguration with SessionUtils {
+class AuthConnector @Inject()(http: Http) extends SessionUtils {
+
+  private val authMicroservice = ConfigFactory.load.getString(s"microservice.external-services.auth-microservice.domain")
+
   def getContext(implicit request: Request[_]): Future[Option[AuthContext]] = {
     http.GET[AuthContext](s"$authMicroservice/get-context/$getContextId") map {
       resp => Some(resp)
